ed812a254db69d6efefd650805a9c32a7d7a82c1
Juuso <juuso.sav@gmail.com>
2026-02-26 09:37:43 +0200

    Small refactorings in Client project

--- GoldenBarbers.Client/Program.cs
+++ GoldenBarbers.Client/Program.cs
@@ -7,19 +7,24 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+// Core
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, LocalAuthStateProvider>();
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Http
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// Domain services
 builder.Services.AddScoped<BarberService>();
-builder.Services.AddScoped<CarouselService>();
 builder.Services.AddScoped<OfferingService>();
 builder.Services.AddScoped<TimeslotService>();
 builder.Services.AddScoped<AppointmentsService>();
+
+// UI helpers
+builder.Services.AddScoped<CarouselService>();
 
 await builder.Build().RunAsync();
