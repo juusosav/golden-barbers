573b548dbf1b86f9ba0224f299f6757b0ee43c3b
Juuso <juuso.sav@gmail.com>
2026-03-31 13:50:59 +0300

    Add: nav.js for mobile navmenu collapse, refactor: CSS for mobile

---
+++ GoldenBarbers.Client/wwwroot/js/nav.js
@@ -0,0 +1,14 @@
+﻿window.navHelper = {
+    closeMenu: function () {
+        const toggler = document.getElementById('nav-toggle');
+        if (toggler) {
+            toggler.checked = false;
+        }
+
+        const navCollapse = document.getElementById('nav-toggle');
+        if (navCollapse && window.bootstrap) {
+            const collapse = bootstrap.Collapse.getInstance(navCollapse);
+            if (collapse) collapse.hide();
+        }
+    }
+}
