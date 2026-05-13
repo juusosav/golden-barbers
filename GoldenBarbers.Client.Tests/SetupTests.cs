9e84d0b2d8073c0aa17f7b423d7912c31be85d94
Juuso <juuso.sav@gmail.com>
2026-05-13 15:18:36 +0300

    Add: Server and Client testing projects setup

---
+++ GoldenBarbers.Client.Tests/SetupTests.cs
@@ -0,0 +1,11 @@
+namespace GoldenBarbers.Client.Tests
+{
+    public class SetupTests
+    {
+        [Fact]
+        public void Setup_IsWorking()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ GoldenBarbers.Server.Tests/SetupTests.cs
@@ -0,0 +1,11 @@
+namespace GoldenBarbers.Server.Tests
+{
+    public class SetupTests
+    {
+        [Fact]
+        public void Setup_IsWorking()
+        {
+            Assert.True(true);
+        }
+    }
+}
